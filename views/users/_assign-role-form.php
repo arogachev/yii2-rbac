3b16236d9dbc05b366fbe1da87c9348a3e28b3c7
Alexey Rogachev <arogachev90@gmail.com>
2015-12-22 15:05:54 +0600

    Fixed typo in drop-down list prompt

--- views/users/_assign-role-form.php
+++ views/users/_assign-role-form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 <?php $form = ActiveForm::begin(); ?>
 
 <?= $form->field($model, 'role')->dropDownList(Rbac::getRolesMap(), [
-    'prompt' => Yii::t('rbac', 'Without any rules'),
+    'prompt' => Yii::t('rbac', 'Without any roles'),
 ]) ?>
 
 <div class="form-group">
